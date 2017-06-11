1e033c9c77a61183b80db1a5f0af45922e6bb0bc
chamy1209 <>
2017-06-11 13:47:00 -0500

    JS-Assessment: Fix count.js

--- app/count.js
+++ app/count.js
@@ -1,7 +1,24 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-  count: function (start, end) {
+    count: function(start, end) {
 
-  }
+        var interval;
+        countNumb();
+
+        function countNumb() {
+
+            if (start <= end) {
+                console.log(start++);
+                interval = setTimeout(countNumb, 100);
+            }
+        }
+
+        return {
+            cancel: function() {
+                clearTimeout(interval);
+            }
+        };
+
+    }
 };
