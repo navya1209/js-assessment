3eb18b18e594c8896bfb0dbc1f9dc58fd31dca83
chamy1209 <>
2017-06-11 13:49:38 -0500

    JS-Assessment: Fix modules.js

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,19 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
-  createModule: function(str1, str2) {
+    createModule: function(str1, str2) {
+        var returnObj = {
 
-  }
+            "greeting": str1,
+            "name": str2,
+
+            "sayIt": function() {
+                return this.greeting + ", " + this.name;
+            }
+
+        };
+
+        return returnObj;
+
+    }
 };
