823cb37ee390d4819d6d9ae20044a0e49cab4ca0
chamy1209 <>
2017-06-11 13:50:50 -0500

    JS-Assessment: Fix objects.js

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,28 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
+    alterContext: function(fn, obj) {
 
-  },
+        return fn.call(obj);
+    },
 
-  alterObjects: function(constructor, greeting) {
+    alterObjects: function(constructor, greeting) {
+        constructor.prototype.greeting = greeting;
 
-  },
+    },
 
-  iterate: function(obj) {
+    iterate: function(obj) {
 
-  }
+        //Get the keys using Object.keys(obj). Iterate and manipulate
+        var result = []
+
+        var keys = Object.keys(obj);
+
+        for (var ind = 0; ind < keys.length; ind++) {
+            result.push(keys[ind] + ': ' + obj[keys[ind]]);
+        }
+
+        return result;
+
+    }
 };
