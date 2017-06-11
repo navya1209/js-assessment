591400f782a18764ed6d4b3eabd6687aed9df460
chamy1209 <>
2017-06-11 13:47:19 -0500

    JS-Assessment: fix bestPractises.js

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -8,19 +8,25 @@ exports = typeof window === 'undefined' ? global : window;
  */
 
 exports.bestPracticesAnswers = {
-  globals: function() {
-    myObject = {
-      name: 'Jory'
-    };
+    globals: function() {
+        var myObject = {
+            name: 'Jory'
+        };
 
-    return myObject;
-  },
+        return myObject;
+    },
 
-  parseInt: function(num) {
-    return parseInt(num);
-  },
+    parseInt: function(num) {
+        return parseInt(num, 10);
+    },
 
-  identity: function(val1, val2) {
+    identity: function(val1, val2) {
 
-  }
+        if (val1 === val2) {
+            return true;
+        }
+
+        return false;
+
+    }
 };
