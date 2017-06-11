fd6cad8aafa256d9dbecb67b120b3ad89730806f
chamy1209 <>
2017-06-11 13:47:51 -0500

    JS-Assessment: Fix numbers.js

--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,51 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
+    valueAtBit: function(num, bit) {
 
-  },
+        //Using inbuilt funcions
+        num = num.toString(2);
 
-  base10: function(str) {
+        //Without using inbuilt functions
+        //     var rem;
+        //  var binary=[];
 
-  },
+        //     while(num > 1) {
 
-  convertToBinary: function(num) {
+        //        rem = num%2;
+        //        num=Math.floor(num/2);
+        //        binary.push(rem);
+        //  }
 
-  },
+        // if(num===1)
+        //      {
+        //        binary.push(1);
+        //      }
+        //   num=binary.join("");
 
-  multiply: function(a, b) {
 
-  }
+        return parseInt(num[(num.length) - bit]);
+
+    },
+
+    base10: function(str) {
+
+        return parseInt(str, 2);
+
+    },
+
+    convertToBinary: function(num) {
+
+        var decialValue = num.toString(2);
+
+        return ('00000000' + decialValue).substring(decialValue.length);
+
+    },
+
+    multiply: function(a, b) {
+        var mulValue = a * b;
+
+        return parseFloat(mulValue.toPrecision(1));
+
+    }
 };
